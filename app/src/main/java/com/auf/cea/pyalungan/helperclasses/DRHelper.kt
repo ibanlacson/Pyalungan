e82bc5e1b83b3e303c0eb870d96d2a589620cfd4
IVANLACSON\Admin <ibanlacson@gmail.com>
2022-10-11 01:19:42 +0800

    Added the helper classes for each game

---
+++ app/src/main/java/com/auf/cea/pyalungan/helperclasses/DRHelper.kt
@@ -0,0 +1,41 @@
+package com.auf.cea.pyalungan.helperclasses
+
+import com.auf.cea.pyalungan.R
+import kotlin.random.Random
+
+class DRHelper {
+    companion object {
+        fun diceRoll():Int{
+            val dice = Random.nextInt(0,7)
+            return when (dice) {
+                (1) -> {
+                    R.drawable.die_1}
+                (2) -> {R.drawable.die_2}
+                (3) -> {R.drawable.die_3}
+                (4) -> {R.drawable.die_4}
+                (5) -> {R.drawable.die_5}
+                (6) -> {R.drawable.die_6}
+                else -> {R.drawable.die_1}
+            }
+        }
+        fun resetLuckyNumber():Int {
+            return Random.nextInt(0,7)
+        }
+        fun evaluateResult(diceRolled:Int, luckyNumber:Int):String {
+            var dice = -1
+            when(diceRolled) {
+                (R.drawable.die_1) -> {dice = 1}
+                (R.drawable.die_2) -> {dice = 2}
+                (R.drawable.die_3) -> {dice = 3}
+                (R.drawable.die_4) -> {dice = 4}
+                (R.drawable.die_5) -> {dice = 5}
+                (R.drawable.die_6) -> {dice = 6}
+            }
+            if(dice == luckyNumber) {
+                return "You got lucky and won!"
+            } else {
+                return "Ouch! Sayang! Better luck next time boss!"
+            }
+        }
+    }
+}
